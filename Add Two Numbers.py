8e18a422cbea7ffe2acf6d15f98a7aea2a112165
lixiaoyang1992 <lixiaoyang2345@gmail.com>
2018-03-29 23:13:54 +0800

    addTwoNumbers

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -1,2 +1,30 @@
-a = 1
-print(a)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        arr = []
+        count = 0
+        while l1 or l2:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            sum += count
+            count = int(sum / 10)
+            sum = sum % 10
+            arr.append(sum)
+        if count > 0:
+            arr.append(count)
+        return arr
